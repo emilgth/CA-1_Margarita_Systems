337968c7277649ff1505f761c0ea56d4f2fb8d5c
Emil <emilgth@gmail.com>
2019-09-10 19:08:17 +0200

    car table done

--- src/main/webapp/carsscript.js
+++ src/main/webapp/carsscript.js
@@ -5,12 +5,22 @@ function createTable() {
     fetch(url)
         .then(res => res.json())
         .then(data => {
+
             let thead = data[0];
-            let table = "<table class='table'><tr>";
+            let table = "<table class='table'><thead><tr>";
             for (let value in thead) {
-                table += `<td>${value}</td>`;
+                table += `<th>${value}</th>`;
             }
-            table += '</tr></table>';
+            table += '</tr></thead><tbody>';
+
+            for (let element of data) {
+                table += '<tr>';
+                for (let value in element) {
+                    table += `<td>${element[value]}</td>`;
+                }
+                table += '</tr>'
+            }
+            table += '</tbody></table>';
             tableDiv.innerHTML = table;
         });
 }
