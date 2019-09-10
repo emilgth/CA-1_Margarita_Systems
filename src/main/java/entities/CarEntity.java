95d3044428cb111c4fcaade04a3e0f73a89f1d10
Emil <emilgth@gmail.com>
2019-09-10 16:34:03 +0200

    Test classes setup -had some trouble with persistence, but now it's fixed :)

--- src/main/java/entities/CarEntity.java
+++ src/main/java/entities/CarEntity.java
@@ -15,6 +15,49 @@ public class CarEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private int year;
+    private String make;
+    private String model;
+    private int price;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public CarEntity(int year, String make, String model, int price) {
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.price = price;
+    }
 
     public CarEntity() {
     }
@@ -28,8 +71,4 @@ public class CarEntity implements Serializable {
     }
 
 
-    
-    
-
-   
 }
