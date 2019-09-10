18133ec2a312292dcfc927e4fe25fd28c2c99992
Emil <emilgth@gmail.com>
2019-09-10 18:59:08 +0200

    table head implemented - also added bootstrap to cars.html

--- src/main/webapp/carsscript.js
+++ src/main/webapp/carsscript.js
@@ -1,1 +1,18 @@
-console.log("Hello!")
+const tableDiv = document.getElementById("table");
+const url = "http://localhost:8080/ca1/api/car/all";
+
+function createTable() {
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            let thead = data[0];
+            let table = "<table class='table'><tr>";
+            for (let value in thead) {
+                table += `<td>${value}</td>`;
+            }
+            table += '</tr></table>';
+            tableDiv.innerHTML = table;
+        });
+}
+
+createTable();
